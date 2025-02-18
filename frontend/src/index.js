50f93e21c7e4de7307edc93113f8b810f1b7b770
yadavrajan68 <98681823+yadavrajan68@users.noreply.github.com>
2025-02-19 00:40:57 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-
->>>>>>> db20a31b48b4c6403188126fb3886bb382db76f8
 import {BrowserRouter} from 'react-router-dom';
 import 'react-toastify/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
